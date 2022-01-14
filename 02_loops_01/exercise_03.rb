39586efff1d1e2831c627715a89d916bf7dcfe0c
richard <richard@richards-MacBook-Pro.local>
2022-01-14 14:52:37 -0500

    added loops_01 exercises 01 to 05

---
+++ 02_loops_01/exercise_03.rb
@@ -0,0 +1,20 @@
+# exercise_03.rb
+
+## Problem - Control the Loop
+# Modify the following loop so it iterates 5 times instead of just once.
+#
+# iterations = 1
+# 
+# loop do
+#   puts "Number of iterations = #{iterations}"
+#   break
+# end
+
+## Solution
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  break if iterations == 5
+  iterations += 1
+end
