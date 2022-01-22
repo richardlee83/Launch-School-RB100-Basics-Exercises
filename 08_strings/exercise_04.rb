21628241c390e6a1f0d2dc2893cb36ed2c0c6ed2
richard <richard@richards-MacBook-Pro.local>
2022-01-21 22:57:43 -0500

    added exercises 04 to 10

---
+++ 08_strings/exercise_04.rb
@@ -0,0 +1,18 @@
+# exercise_04.rb
+
+## Problem - Dynamic String
+# Modify the following code so that the value of name is printed within "Hello, !".
+#
+# name = 'Elizabeth'
+#
+# puts "Hello, !"
+#
+# Expected output:
+#
+# Hello, Elizabeth!
+
+## Solution
+name = "Elizabeth"
+
+puts "Hello, #{name}!
+
---
+++ 08_strings/exercise_05.rb
@@ -0,0 +1,23 @@
+# exercise_05.rb
+
+## Problem - Combining Names
+#
+# Using the following code, combine the two names together to form a full name and 
+# assign that value to a variable named full_name. Then, print the value of full_name.
+#
+# first_name = 'John'
+# last_name = 'Doe'
+#
+# Expected output:
+#
+# John Doe
+
+## Solution
+first_name = "John"
+last_name = "Doe"
+
+full_name = first_name + " " + last_name
+
+p full_name
+
+
