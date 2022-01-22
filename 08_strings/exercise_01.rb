ee0071f09c3bf823218a1e812dafe71189e3b3cb
richard <richard@richards-MacBook-Pro.local>
2022-01-21 20:29:32 -0500

    added exercises 01 to 03

---
+++ 08_strings/exercise_01.rb
@@ -0,0 +1,10 @@
+# exercise_01.rb
+
+## Problem - Create a String
+#
+# Create an empty string using the String class and assign it to a variable.
+#
+
+## Solution
+str = String.new("")
+p [str, str.length, str.class]    #  ["", 0, String]
