3206d4978509d02643ea00a3f2e79373b1da4f52
richard <richard@richards-MacBook-Pro.local>
2022-01-23 21:50:17 -0500

    added exercises 07 to 10

---
+++ 09_arrays/exercise_10.rb
@@ -0,0 +1,21 @@
+# exercise_10.rb
+
+# Problem - Are We The Same?
+#
+# In the code below, two arrays containing several numbers are assigned to 
+# two variables, array1 and array2.
+#
+# array1 = [1, 5, 9]
+# array2 = [1, 9, 5]
+#
+# Compare array1 and array2 and print true or false based on whether they match.
+
+# Solution
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+p array1 == array2    # false
+
+
+
+
